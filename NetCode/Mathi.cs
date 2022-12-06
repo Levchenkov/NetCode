0afe76b0502bfa10de6dd105e5ec9ff60cf919e2
Eugene Levchenkov <levchenkov.eugene@gmail.com>
2022-12-06 11:02:53 +0200

    Update TargetFrameworks

--- NetCode/Mathi.cs
+++ NetCode/Mathi.cs
@@ -7,12 +7,19 @@ public static class Mathi
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static int Ceiling(int a, int b)
     {
-        var (quotient, remainder) = Math.DivRem(a, b);
+        var (quotient, remainder) = DivRem(a, b);
         if (remainder == 0)
         {
             return quotient;
         }
 
         return quotient + 1;
+    }
+    
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static (int Quotient, int Remainder) DivRem(int left, int right)
+    {
+        int quotient = left / right;
+        return (quotient, left - (quotient * right));
     }
 }
